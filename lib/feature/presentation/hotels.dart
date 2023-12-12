cb20f43ea22cd44cddae5bc2a89fb3031855f1b3
orekisday <nurmukhammed.ernestov@alatoo.edu.kg>
2023-12-12 21:16:02 +0600

    screen res adjustments

--- lib/feature/presentation/hotels.dart
+++ lib/feature/presentation/hotels.dart
@@ -59,9 +59,11 @@ class _HotelsState extends State<Hotels> {
                 const SizedBox(
                   width: 330,
                   child: Text(
-                      """Maison is a new, modern club hotel located 150 meters from the beach of the high mountain, crystal clear lake Issyk-Kul, in the village of Bosteri - in the center of the resort area. We do not have our own beach, but we are very close to the beach of the resort town and the beach of the Golden Sands boarding house.
+                    """Maison is a new, modern club hotel located 150 meters from the beach of the high mountain, crystal clear lake Issyk-Kul, in the village of Bosteri - in the center of the resort area. We do not have our own beach, but we are very close to the beach of the resort town and the beach of the Golden Sands boarding house.
                       \nThe best restaurants, cafes and clubs of Bosteria are within walking distance.
-                      \nThis is the perfect place to relax for you and your family. We are waiting for you to visit, to accumulate memories in your memory piggy bank.""", style: TextStyle(color: Colors.white),),
+                      \nThis is the perfect place to relax for you and your family. We are waiting for you to visit, to accumulate memories in your memory piggy bank.""",
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
                 const SizedBox(
                   height: 20,
@@ -69,15 +71,23 @@ class _HotelsState extends State<Hotels> {
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image.asset(Images.image7, width: 95,height: 95,),
+                    Image.asset(
+                      Images.image7,
+                      width: 95,
+                      height: 95,
+                    ),
                     const SizedBox(
                       width: 9,
                     ),
                     const Column(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        SizedBox(width: 216, child: Text("Maison\nHOTEL & RESTAURANT", style: TextStyle(color: Colors.white),)),
-                        SizedBox(width: 216, child: Text("Our hotel is located just a five-minute walk from the beach so you can immerse yourself in fresh waters and indulge in the sunshine. Don't worry about water and electricity - we have an uninterrupted supply!", style: TextStyle(color: Colors.white),))
+                        SizedBox(
+                            width: 200,
+                            child: Text(
+                              "Maison\nHOTEL & RESTAURANT\nOur hotel is located just a five-minute walk from the beach so you can immerse yourself in fresh waters and indulge in the sunshine. Don't worry about water and electricity - we have an uninterrupted supply!",
+                              style: TextStyle(color: Colors.white),
+                            )),
                       ],
                     )
                   ],
@@ -85,7 +95,6 @@ class _HotelsState extends State<Hotels> {
                 const SizedBox(
                   height: 20,
                 ),
-                
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -106,71 +115,74 @@ class _HotelsState extends State<Hotels> {
                 const SizedBox(
                   width: 330,
                   child: Text(
-                      """Booking and accommodation at the Maison Hotel is in accordance with international standards.
+                    """Booking and accommodation at the Maison Hotel is in accordance with international standards.
                       \nCheck-in time is 13 hours
                       \nCheck-out time is 12 hours
                       \nEarly check-in is possible only subject to availability and at no extra charge.
 Late check-out, after 12:00 to 00:00, is regarded as 50% of the room rate.
                       \nCheck availability, make a prepayment to your Optima Bank account or card and anticipate a trip to Issyk-Kul.
-Have a good rest!""", style: TextStyle(color: Colors.white),),
+Have a good rest!""",
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(
-                    top: 11, right: 200
-                  ),
-                  child: SignInBtn(onPressed: (){}, text: "Booking")
-                ),
+                    padding: const EdgeInsets.only(top: 11, right: 200),
+                    child: SignInBtn(onPressed: () {}, text: "Booking")),
                 const SizedBox(
                   height: 17,
                 ),
                 Row(
                   children: [
-                    Row( 
+                    Row(
                       children: [
                         SvgPicture.asset(Images.location),
                         const SizedBox(
                           width: 8,
                         ),
                         const Text(
-                          """Location\nNear to the Ferris Wheel, p. Bosteri""",
+                          """Location\nNear to the Ferris Wheel, \n p. Bosteri""",
                           style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10
-                          ),
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10),
                         ),
                         const SizedBox(
                           width: 17,
                           height: 17,
                         ),
                         Image.asset(
-                        Images.image10,
-                        width: 99.5,
-                        height: 85,
+                          Images.image10,
+                          width: 99.5,
+                          height: 85,
                         ),
                       ],
-                    ),                  
+                    ),
                   ],
                 ),
                 const SizedBox(
                   height: 17,
                 ),
                 Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Image.asset(Images.image11, width: 95,height: 95,),
-                        const SizedBox(
-                          width: 17,
-                        ),
-                        const SizedBox(
-                          width: 200,
-                          child: Text(
-                            """Now or never!\nHurry up and catch a ticket for a happy summer,\njump into the car and\ncome to the Maison Hotel to live the brightest moments of this summer 
-\nAnd we will be happy to help you with this\n\nContacts:\n+996 501 799 977\ninstagram:\nmaison.ik""", style: TextStyle(color: Colors.white),
-                          ),
-                        )
-                      ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      Images.image11,
+                      width: 100,
+                      height: 100,
                     ),
+                    const SizedBox(
+                      width: 17,
+                    ),
+                    const SizedBox(
+                      width: 150,
+                      child: Text(
+                        """Now or never!\nHurry up and catch a ticket for a happy summer,\njump into the car and\ncome to the Maison Hotel to live the brightest moments of this summer 
+\nAnd we will be happy to help you with this\n\nContacts:\n+996 501 799 977\ninstagram:\nmaison.ik""",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ),
